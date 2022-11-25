a0464f4d015b5326b9fc3a0488625c3bfd768073
zbigniew <zbyszski@gmail.com>
2022-11-25 18:40:57 +0100

    25.11.2022.01 - imgaes, initialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-// import { getCookie } from "cookies-next";
+import { getCookie } from "cookies-next";
 import { SnackbarProvider } from "notistack";
 import { useState } from "react";
 import PageProvider from "../utils/helpers/PageProvider";
 import { StoreProvider } from "../utils/Store";
 
 function App(props) {
-  const { Component, pageProps /*initialTheme*/ } = props;
-  const [currentTheme, setCurrentTheme] = useState(/*initialTheme*/ "light");
+  const { Component, pageProps, initialTheme } = props;
+  const [currentTheme, setCurrentTheme] = useState(initialTheme);
   const setThemeHandler = (v) => setCurrentTheme(v);
   return (
     <SnackbarProvider anchorOrigin={{ vertical: "top", horizontal: "center" }}>
@@ -26,12 +26,12 @@ function App(props) {
   );
 }
 
-// App.getInitialProps = async ({ ctx }) => {
-//   return {
-//     initialTheme: getCookie("darkMode", ctx)
-//       ? await JSON.parse(getCookie("darkMode", ctx))
-//       : "light",
-//   };
-// };
+App.getInitialProps = async ({ ctx }) => {
+  return {
+    initialTheme: getCookie("darkMode", ctx)
+      ? await JSON.parse(getCookie("darkMode", ctx))
+      : "light",
+  };
+};
 
 export default App;
